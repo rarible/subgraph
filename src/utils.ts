3e7b53ffddf55c116127904b983705add24a84ea
aesariyev <asariyev@gmail.com>
2020-12-24 12:58:44 +0600

    fix price and fee calculations

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,16 @@
 import { Deal } from "../generated/schema"
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts/index"
-import { DealType, Coefficient } from "./enum"
+import { Coefficient } from "./enum"
 
 export function calculatePriceAndFee(deal: Deal): void {
-    deal.price = calculatePrice(deal.buyAmount, deal.sellAmount, deal.type)
-    deal.fee = calculateFee(deal.sellAmount, deal.price.toBigDecimal())
+    deal.price = calculatePrice(deal.buyAmount, deal.sellAmount)
+    deal.fee = calculateFee(deal.buyAmount)
 }
 
-function calculatePrice(buyAmount: BigInt, sellAmount: BigInt, dealType: string): BigInt {
-    if (dealType == DealType.ORDER){
-        return buyAmount / sellAmount
-    } else {
-        return sellAmount / buyAmount
-    }
+function calculatePrice(buyAmount: BigInt, sellAmount: BigInt): BigInt {
+    return buyAmount / sellAmount
 }
 
-function calculateFee(sellAmount: BigInt, price: BigDecimal): BigDecimal {
-    return sellAmount.toBigDecimal() * price * BigDecimal.fromString(Coefficient.FEE)
+function calculateFee(buyAmount: BigInt): BigDecimal {
+    return buyAmount.toBigDecimal() * BigDecimal.fromString(Coefficient.FEE)
 }
